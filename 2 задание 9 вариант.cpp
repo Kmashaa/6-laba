237d8c1191f7f9acdf488ac5cf6af6379b064bc2
Kmashaa <74793818+Kmashaa@users.noreply.github.com>
2020-12-18 21:29:48 +0300

    Add files via upload

---
+++ 2 задание 9 вариант.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() 
+{
+	string str;
+	cout << "enter str:" << endl;
+	int countVowels = 0;
+	int countConsonants = 0;
+
+	getline(cin, str);
+	for (int i = 0; i < (int)size(str); i++)
+	{
+		if(str[i]=='A'|| str[i] == 'a' || str[i] == 'E' || str[i] == 'e' || str[i] == 'Y' || str[i] == 'y' || str[i] == 'U' || str[i] == 'u' || str[i] == 'I' || str[i] == 'i' || str[i] == 'O' || str[i] == 'o')
+		{
+			countVowels++;
+		}
+		else if(((str[i] > char(64) && str[i]<char(91)) || (str[i] > char(96) && str[i]<char(123)))&& (str[i] != 'A' && str[i] != 'a' && str[i] != 'E' && str[i] != 'e' && str[i] != 'Y' && str[i] != 'y' && str[i] != 'U' && str[i] != 'u' && str[i] != 'I' && str[i] != 'i' && str[i] != 'O' && str[i] != 'o'))
+		{ 
+			countConsonants++; 
+		}
+	}
+	if (countVowels > countConsonants) { cout << "There are more vowels than consonants"; }
+	else if(countVowels < countConsonants) { cout << "There are more consonants than vowels"; }
+	else if (countVowels == countConsonants) { cout << "There are as many vowels as consonants"; }
+}
